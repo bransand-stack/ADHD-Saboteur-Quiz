bbf05a3708076657eecaf9234a098db084604688
bransand-stack <bransand@bransandservices.com>
2026-03-06 12:53:06 -0500

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SaboteurQuiz from './SaboteurQuiz'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SaboteurQuiz />
+  </React.StrictMode>
+)
