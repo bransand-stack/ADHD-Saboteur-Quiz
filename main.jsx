22c988565a4073e90597cde2b50c8c7791976c22
bransand-stack <bransand@bransandservices.com>
2026-03-06 13:01:51 -0500

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import SaboteurQuiz from './SaboteurQuiz'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <SaboteurQuiz />
-  </React.StrictMode>
-)
